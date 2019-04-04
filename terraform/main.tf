1a016fc1a0930d0a776bfc2a743184f921709505
hardcore21 <48283031+hardcore21@users.noreply.github.com>
2019-04-04 16:00:34 +0300

    parameters connecting to provisioners

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,36 @@
 provider "google" {
-  project = "infra-236514"
-  region = "europe-west1"
-  }
+   project = "infra-236514"
+   region  = "europe-west1"
+}
 resource "google_compute_instance" "app" {
-name = "reddit-app"
-machine_type = "g1-small"
-zone = "europe-west1-b"
-
-tags = ["reddit-app"]
-
-metadata {
-	sshKeys = "appuser:${file ("~/.ssh/id_rsa.pub")}"
-	}
-# определение загрузочного диска
-boot_disk {
-  initialize_params {
-    image = "reddit-base-1554306998"
-  }
-}
-# определение сетевого интерфейса
-network_interface {
-# сеть, к которой присоединить данный интерфейс
-network = "default"
-# использовать ephemeral IP для доступа из Интернет
-access_config {}
-  }
-}
-resource "google_compute_firewall" "firewall_puma" {
-name = "allow-puma-default"
-# Название сети, в которой действует правило
-network = "default"
-# Какой доступ разрешить
-allow {
-	protocol = "tcp"
-	ports = ["9292"]
-	}
-# Каким адресам разрешаем доступ
-source_ranges = ["0.0.0.0/0"]
-# Правило применимо для инстансов с тегом …
-target_tags = ["reddit-app"]  
-}
-
+   name         = "reddit-app"
+   machine_type = "g1-small"
+   zone         = "europe-west1-b"
+   boot_disk {
+      initialize_params {
+      image = "reddit-base-1554306998"
+      }
+   }
+   metadata {
+	sshKeys = "appuser:${file ("~/.ssh/appuser.pub")}"
+   }
+   tags = ["reddit-app"]
+   network_interface {
+     network = "default"
+     access_config {}
+   }
+   connection {
+     type = "ssh"
+     user = "appuser"
+     agent = false
+     private_key = "${file("~/.ssh/appuser")}"
+   }	
 provisioner "file" {
-	source 		= "files/puma.service"
-	destination	= "/tmp/puma.service"
+	source 	    = "files/puma.service"
+	destination = "/tmp/puma.service"
 	}
 provisioner "remote-exec" {
-	script 		= "files/deploy.sh"
+	script 	= "files/deploy.sh"
 	}
-
+}
 
