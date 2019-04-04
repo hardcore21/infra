c7f1eb3520d5ef8c55f12a86b52b8b39ab668581
Dima Malets <maletsd@gmail.com>
2019-04-04 14:01:30 +0300

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,9 @@ resource "google_compute_instance" "app" {
 name = "reddit-app"
 machine_type = "g1-small"
 zone = "europe-west1-b"
+metadata {
+	sshKeys = "appuser:${file ("~/.ssh/id_rsa.pub")}"
+	}
 # определение загрузочного диска
 boot_disk {
   initialize_params {
