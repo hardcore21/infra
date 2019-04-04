1402312299abb195b21392ebed4a84894d5cb83a
hardcore21 <48283031+hardcore21@users.noreply.github.com>
2019-04-04 13:29:05 +0300

    resource for instance VM

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,21 @@ provider "google" {
   project = "infra-236514"
   region = "europe-west1"
   }
+resource "google_compute_instance" "app" {
+name = "reddit-app"
+machine_type = "g1-small"
+zone = "europe-west1-b"
+# определение загрузочного диска
+boot_disk {
+  initialize_params {
+    image = "reddit-base-1554306998"
+  }
+}
+# определение сетевого интерфейса
+network_interface {
+# сеть, к которой присоединить данный интерфейс
+network = "default"
+# использовать ephemeral IP для доступа из Интернет
+access_config {}
+  }
+}
