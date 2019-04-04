eae1cb52626b6436dcf105250e2c1336aa2b38eb
hardcore21 <48283031+hardcore21@users.noreply.github.com>
2019-04-04 14:40:45 +0300

    add port for app

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,4 +22,18 @@ network = "default"
 # использовать ephemeral IP для доступа из Интернет
 access_config {}
   }
+resource "google_compute_firewall" "firewall_puma" {
+name = "allow-puma-default"
+# Название сети, в которой действует правило
+network = "default"
+# Какой доступ разрешить
+allow {
+	protocol = "tcp"
+	ports = ["9292"]
+	}
+# Каким адресам разрешаем доступ
+source_ranges = ["0.0.0.0/0"]
+# Правило применимо для инстансов с тегом …
+target_tags = ["reddit-app"]
+  }
 }
